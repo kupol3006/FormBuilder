591b869bcaa02bdbc27609115aabd37b34f1db02
kupol3006 <truongdieuphat@gmail.com>
2024-08-20 11:42:37 +0700

    1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import webpack from 'next/dist/compiled/webpack/webpack-lib.js';
+
+const nextConfig = {
+  webpack: (config) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+      })
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
