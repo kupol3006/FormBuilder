1c151bee60186cd1a88e733afe4d42351540ea78
kupol3006 <truongdieuphat@gmail.com>
2024-09-06 10:52:00 +0700

    6/9

--- src/app/Form/page.js
+++ src/app/Form/page.js
@@ -1,9 +1,6 @@
 'use client';
 import { useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import 'jquery-ui/ui/widgets/sortable';
-import formBuilder from 'formBuilder';
 import { useSelector } from 'react-redux';
 
 export default function FormPage() {
@@ -12,20 +9,35 @@ export default function FormPage() {
 
   useEffect(() => {
     if (typeof window !== 'undefined' && data) {
-      $(function() {
-        console.log("jQuery is ready!");
+      import('jquery').then(($) => {
+        window.$ = window.jQuery = $;
+        return import('jquery-ui/ui/widgets/sortable');
+      }).then(() => {
+        return import('formBuilder');
+      }).then(() => {
+        return import('formBuilder/dist/form-render.min.js');
+      }).then(() => {
+        if (typeof $ === 'function') {
+          $(function() {
+            console.log("jQuery is ready!");
 
-        $('.fb-render').formRender({
-          dataType: 'xml',
-          formData: data
-        });
+            $('.fb-render').formRender({
+              dataType: 'xml',
+              formData: data
+            });
+          });
+        } else {
+          console.error("jQuery is not loaded properly.");
+        }
+      }).catch((error) => {
+        console.error("Error loading dependencies:", error);
       });
     }
   }, [data]);
 
   return (
     <div>
-      <textarea id="fb-template" ref={fbTemplateRef} style={{ display: 'none' }} value={data}></textarea>
+      <textarea id="fb-template" ref={fbTemplateRef} style={{ display: 'none' }} defaultValue={data}></textarea>
       <div className="fb-render"></div>
     </div>
   );
